bbe7db0d63174ae6eb4f28f4684776a9eb85161f
Say <reubenlouis64@gmail.com>
2025-12-02 13:52:41 +0800

    Implement specification pattern.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,47 +1,55 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ProductsController(IProductRepository productRepository) : ControllerBase
+public class ProductsController(IGenericRepository<Product> productRepository) : ControllerBase
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        return Ok(await productRepository.GetProductsAsync(brand, type, sort));
+        var specification = new ProductSpecification(brand, type, sort);
+        var products = await productRepository.GetAllWithSpec(specification);
+
+        return Ok(products);
     }
 
     [HttpGet("{id:int}")] // api/products/1
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await productRepository.GetProductByIdAsync(id);
+        var product = await productRepository.GetByIdAsync(id);
         return product == null ? NotFound() : product;
     }
 
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
     {
-        return Ok(await productRepository.GetBrandsAsync());
+        var specification = new BrandListSpecification();
+        var products = await productRepository.GetAllWithSpec(specification);
+        return Ok(products);
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
     {
-        return Ok(await productRepository.GetTypesAsync());
+        var specification = new TypeListSpecification();
+        var products = await productRepository.GetAllWithSpec(specification);
+        return Ok(products);
     }
 
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        productRepository.AddProduct(product);
+        productRepository.Add(product);
 
-        if (await productRepository.SaveChangesAsync())
+        if (await productRepository.SaveAllAsync())
             return CreatedAtAction("GetProduct", new { id = product.Id }, product);
 
-        await productRepository.SaveChangesAsync();
+        await productRepository.SaveAllAsync();
         return BadRequest("Error creating the product");
     }
 
@@ -51,8 +59,8 @@ public class ProductsController(IProductRepository productRepository) : Controll
         if (product.Id != id || !_DoesProductExists(id))
             return BadRequest("Cannot update this product!");
 
-        productRepository.UpdateProduct(product);
-        if (await productRepository.SaveChangesAsync()) return NoContent();
+        productRepository.Update(product);
+        if (await productRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Error updating the product");
     }
@@ -60,18 +68,18 @@ public class ProductsController(IProductRepository productRepository) : Controll
     [HttpDelete("{id:int}")] // api/products/1
     public async Task<ActionResult> DeleteProduct(int id)
     {
-        var productToRemove = await productRepository.GetProductByIdAsync(id);
+        var productToRemove = await productRepository.GetByIdAsync(id);
 
         if (productToRemove == null) return NotFound();
 
-        productRepository.DeleteProduct(productToRemove);
-        if (await productRepository.SaveChangesAsync()) return NoContent();
+        productRepository.Remove(productToRemove);
+        if (await productRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Error deleting the product");
     }
 
     private bool _DoesProductExists(int id)
     {
-        return productRepository.ProductExists(id);
+        return productRepository.Exists(id);
     }
 }
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T?> GetEntityWithSpec(ISpecification<T> specification);
+    Task<IReadOnlyList<T>> GetAllWithSpec(ISpecification<T> specification);
+    Task<TResult?> GetEntityWithSpec<TResult>(ISpecification<T, TResult> specification);
+    Task<IReadOnlyList<TResult>> GetAllWithSpec<TResult>(ISpecification<T, TResult> specification);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> SaveAllAsync();
+    bool Exists(int id);
+}
