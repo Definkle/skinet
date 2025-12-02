c527e462f2fcc708620a0b6bfafb6469d531c220
Say <reubenlouis64@gmail.com>
2025-12-02 17:37:06 +0800

    Add error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,37 @@
+﻿using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("badRequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Bad request");
+    }
+
+    [HttpGet("notFound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("internalError")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("Internal error");
+    }
+
+    [HttpPost("validationError")]
+    public IActionResult GetValidationError(CreateProductDto product)
+    {
+        return Ok();
+    }
+}
