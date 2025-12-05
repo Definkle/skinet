6496fd1b398a6b47fa71c89fd0d9115a9421893e
Say <reubenlouis64@gmail.com>
2025-12-05 21:53:47 +0800

    Add shopping cart API

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface ICartService
+{
+    Task<ShoppingCart?> GetShoppingCartAsync(string key);
+    Task<ShoppingCart?> SetShoppingCartAsync(ShoppingCart cart);
+    Task<bool> DeleteAsync(string key);
+}
---
+++ Infrastructure/Services/CartService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+
+namespace Infrastructure.Services;
+
+public class CartService(IConnectionMultiplexer redis) : ICartService
+{
+    private readonly IDatabase _database = redis.GetDatabase();
+
+    public async Task<ShoppingCart?> GetShoppingCartAsync(string key)
+    {
+        var data = await _database.StringGetAsync(key);
+
+        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<ShoppingCart>(data.ToString());
+    }
+
+    public async Task<ShoppingCart?> SetShoppingCartAsync(ShoppingCart cart)
+    {
+        var created = await _database.StringSetAsync(cart.Id, JsonSerializer.Serialize(cart), TimeSpan.FromDays(30));
+
+        return !created ? null : await GetShoppingCartAsync(cart.Id);
+    }
+
+    public async Task<bool> DeleteAsync(string key)
+    {
+        return await _database.KeyDeleteAsync(key);
+    }
+}
